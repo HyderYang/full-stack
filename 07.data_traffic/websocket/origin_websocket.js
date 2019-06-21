8b87a4477a3990c98262de22aaabd7236f05551b
hyder <yanghuan0613@gmail.com>
2019-06-22 00:33:57 +0800

    完成

--- 07.data_traffic/websocket/origin_websocket.js
+++ 07.data_traffic/websocket/origin_websocket.js
@@ -1,9 +1,52 @@
 const net = require('net');
+const crypto = require('crypto');
+
+function parseHeader(str){
+  let arr = str.split('\r\n').filter((line) => {
+      return line;
+  });
+
+  arr.shift();
+  let header = {};
+  arr.forEach((line) => {
+      let [name, value] = line.split(': ');
+      name = name.replace(/^\s+|\s+$/g, '').toLowerCase();
+      value = value.replace(/^\s+|\s+$/g, '');
+      header[name] = value;
+  });
+  return header;
+}
 
 const server = net.createServer((socket) => {
   socket.once('data', (buffer) => {
     let str = buffer.toString();
-    console.log(str);
+    let headerArr = parseHeader(str);
+
+    if (headerArr['upgrade'] !== 'websocket') {
+
+      console.log('no upgrade');
+      socket.end();
+
+    } else if (headerArr['sec-websocket-version'] !== '13'){
+
+      console.log('not version 13');
+      socket.end();
+
+    } else {
+
+      let key = headerArr['sec-websocket-key'];
+      let uuid = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+      let hash = crypto.createHash('sha1');
+
+      hash.update(key + uuid);
+      const finalKey = hash.digest('base64');
+
+      let writeHeader = 'HTTP/1.1 101 Switching Protocols\r\n'
+        + 'Upgrade: websocket\r\n'
+        + 'Connection: upgrade\r\n'
+        + 'Sec-Websocket-Accept: ' + finalKey + '\r\n\r\n';
+      socket.write(writeHeader);
+    }
   });
 
   socket.on('end', () => {
