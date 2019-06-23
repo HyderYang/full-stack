2900eb0682a611337e97472adc7cc9710fc5d018
hyder <yanghuan0613@gmail.com>
2019-06-23 22:40:32 +0800

    express router

--- 10.express/server.js
+++ 10.express/server.js
@@ -5,7 +5,12 @@ server.listen(8989);
 
 server.get('/a', (req, res, next) => {
    // res.send('aaa');
-  res.send({a: 'a'})
+  // res.send({a: 'a'});
+  console.log('aaa');
+  next();
 });
 
-
+server.get('/a', (req, res, next) => {
+  // res.send('aaa');
+  console.log('bbb')
+});
