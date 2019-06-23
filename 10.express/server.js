73843452811952a7fc12c592ebc4eada1d46c6fd
hyder <yanghuan0613@gmail.com>
2019-06-23 23:18:31 +0800

    express 中间件

--- 10.express/server.js
+++ 10.express/server.js
@@ -1,12 +1,28 @@
 const express = require('express');
-
+const body = require('body-parser');
+const querystring = require('querystring');
 const server = express();
 server.listen(8989);
 
+// server.use(body.urlencoded({
+//   extended: false
+// }));
+
+server.use((req, res, next) => {
+  let arr = [];
+  req.on('data', buffer => {
+    arr.push(buffer);
+  });
+
+  req.on('end', () => {
+      req.body = querystring.parse(Buffer.concat(arr).toString());
+  });
+  next();
+});
+
 server.get('/a', (req, res, next) => {
-   // res.send('aaa');
-  // res.send({a: 'a'});
-  console.log('aaa');
+  // 解析get数据
+  console.log(req.query);
   next();
 });
 
@@ -14,3 +30,9 @@ server.get('/a', (req, res, next) => {
   // res.send('aaa');
   console.log('bbb')
 });
+
+server.post('/reg', (req, res) => {
+    console.log(req.body);
+});
+
+server.use(express.static('./static/'));
