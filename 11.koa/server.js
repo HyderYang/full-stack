a8cb2494cde513f241b8deca064320e5fec8dc3a
hyder <yanghuan0613@gmail.com>
2019-06-24 01:07:39 +0800

    koa router

--- 11.koa/server.js
+++ 11.koa/server.js
@@ -1,4 +1,13 @@
 const koa = require('koa');
+const Router = require('koa-router');
 
 const server = new koa();
 server.listen(8989);
+
+const router = new Router();
+
+router.get('/a', async (ctx, next) => {
+  ctx.body = 'aaaa';
+});
+
+server.use(router.routes());
