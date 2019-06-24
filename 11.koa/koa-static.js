4319426901da968d84fe738aff520ae8333d92cd
hyder <yanghuan0613@gmail.com>
2019-06-24 21:41:06 +0800

    static bug fix

--- 11.koa/koa-static.js
+++ 11.koa/koa-static.js
@@ -1,6 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-const static = require('koa-static');
+const statics = require('koa-static');
 
 const server = new Koa();
 server.listen(8989);
@@ -12,26 +12,26 @@ route.get('user', (ctx) => {
 
 server.use(route.routes());
 
-const staticRouter = new Router();
-staticRouter.all(/(\.jpg|\.png|\.gif)$/i, static('./static', {
+const staticsRouter = new Router();
+staticsRouter.all(/(\.jpg|\.png|\.gif)$/i, statics('./static', {
   maxage: 60 * 86400 * 1000
 }));
 
-staticRouter.all(/\.css$/i, static('./static', {
+staticsRouter.all(/\.css$/i, statics('./static', {
   maxage: 86400 * 1000
 }));
 
-staticRouter.all(/(\.html|\.htm|\.shtml)$/i, static('./static', {
+staticsRouter.all(/(\.html|\.htm|\.shtml)$/i, statics('./static', {
   maxage: 20 * 86400 * 1000
 }));
 
 // 其他文件缓存
-staticRouter.all('', static('./static', {
+staticsRouter.all('', statics('./static', {
   maxage: 30 * 86400 * 1000
 }));
 
-server.use(staticRouter.routes());
-// server.use(static('./static', {
+server.use(staticsRouter.routes());
+// server.use(statics('./statics', {
 //   maxage: 86400 * 1000, // client 缓存时间
 //   index: '1.html' // 默认文件
 // }));
