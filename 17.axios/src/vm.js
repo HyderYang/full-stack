66305a4fd5bd9873ed05cef8da68e1176ea3a9d6
hyder <yanghuan0613@gmail.com>
2019-07-02 23:47:41 +0800

    axios

--- 17.axios/src/vm.js
+++ 17.axios/src/vm.js
@@ -1,3 +1,22 @@
 import Vue from 'vue/dist/vue.esm'
 import Axios from 'axios'
 
+const vm = new Vue({
+  el: '#div1',
+
+  data: {
+    name: '',
+    age: 0
+  },
+
+  created() {
+
+  },
+
+  template: `
+    <div>
+      <label>名字: </label><span>{{name}}</span><br />
+      <label>年龄: </label><span>{{age}}</span>  
+    </div>
+  `
+});
