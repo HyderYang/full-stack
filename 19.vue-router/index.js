5bc7e23ffb947366da795b8be04a96876e38ef28
hyder <yanghuan0613@gmail.com>
2019-07-04 22:42:46 +0800

    vue-loader end

--- 19.vue-router/index.js
+++ 19.vue-router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import App from './App.vue';
 import router from './src/router';
+import Axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, Axios);
 
 new Vue({
   el: '#app',
