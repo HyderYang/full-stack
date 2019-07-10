4c80e81d3486fd15d8448a195729f3b0f244994d
hyder <yanghuan0613@gmail.com>
2019-07-10 22:59:45 +0800

    antd按需加载override

---
+++ 22.react基础/antd/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Button} from 'antd'
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Button type='primary'>antd</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
